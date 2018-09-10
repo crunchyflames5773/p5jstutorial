06f3654b30d9a95b28d037f97800512a70c0d087
crunchyflames5773 <42793309+crunchyflames5773@users.noreply.github.com>
2018-09-10 12:55:29 +0800

    Add files via upload

---
+++ p5jsTutorial1.3-master (Current)/sketch.js
@@ -0,0 +1,43 @@
+function setup()
+{
+  createCanvas(700,500);
+  
+}
+
+function draw()
+{
+	background(255,0,0)
+
+	fill(0,255,255);
+	stroke(0,0,0);
+	strokeWeight(3);
+
+
+	text("Hello World",120,30);
+	text("Hello World",20,80);
+    text("Hello World",220,80);
+    text("Hello World",120,130);
+    text("Hello World",320,30);
+    text("Hello World",320,130);
+	
+	rect(100,50,100,50);
+	rect(0,0,100,50);
+	rect(0,100,100,50);
+    rect(200,100,100,50);
+    rect(200,0,100,50);
+    rect(300,50,100,50);
+    rect(300,50,100,50);
+
+	
+
+	fill(0,0,255);
+
+	text("Donald Trump",10,30);
+	text("Donald Trump",110,80);
+	text("Donald Trump",210,30);
+	text("Donald Trump",210,130);
+	text("Donald Trump",210,130);
+	text("Donald Trump",310,80);
+	text("Donald Trump",10,130);
+
+}
